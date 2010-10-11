f5766ddca0a7810c1debc6976e584915dc82d141
masaedw <masa.edw@gmail.com>
2010-10-11 22:52:50 +0900

    project description and swank

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject mandelbrotter "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject mandelbrotter "0.1.0-SNAPSHOT"
+  :description "draw mandelbrot"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
