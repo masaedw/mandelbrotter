07ca2a9fdde43ea124dab50caf4a47034b576a74
masaedw <masa.edw@gmail.com>
2010-10-12 21:59:24 +0900

    use proxy

--- src/mandelbrotter/core.clj
+++ src/mandelbrotter/core.clj
@@ -1,8 +1,38 @@
-(ns mandelbrotter.core)
+(ns mandelbrotter.core
+  (:import (java.awt Dimension)
+           (javax.swing JPanel JFrame)
+           (java.awt.event MouseListener)))
+
+(defn paint [g mandelbrot]
+  )
+
+(defn main-panel
+  [mandelbrot]
+  (proxy [JPanel MouseListener] []
+    (paintComponent [g]
+      (proxy-super paintComponent g)
+      (paint g @mandelbrot))
+    (mousePressed [e])
+    (mouseReleased [e])
+    (mouseEntered [e])
+    (mouseExited [e])
+    (mouseClicked [e]
+      (.repaint this))))
+
+(defn create-mandelbrot
+  []
+  {})
 
 (defn main-
   []
-  (let [frame (mandelbrotter.MainFrame.)]
+  (let [mandel (ref (create-mandelbrot))
+        frame (JFrame. "mandelbrot")
+        panel (main-panel mandel)]
+    (doto panel
+      (.setPreferredSize (Dimension. 800 600))
+      (.addMouseListener panel))
     (doto frame
-    ;;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.setVisible true))))
+      (.add panel)
+      (.pack)
+      (.setVisible true))
+    [mandel]))
