1192d36ac6bcb6dea3d5e6e002d21a074caf4ba9
masaedw <masa.edw@gmail.com>
2010-12-11 18:43:48 +0900

    memory

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "draw mandelbrot"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :jvm-opts ["-Xmx1g"])
